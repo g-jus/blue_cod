d13ae4c2dc03ea7d0cb9cd0cca571a1b8cd81ecc
g-jus <gusun5199@uib.no>
2026-02-09 13:05:41 +0100

    a short demo on penguins

---
+++ penguins_demo.R
@@ -0,0 +1,16 @@
+library(palmerpenguins)
+library(tidyverse)
+ggplot(penguins, aes(x = species, y = bill_length_mm, fill = species)) + 
+  geom_violin() + 
+  scale_color_viridis_d() +
+  labs(
+    x = "Bill length mm",
+    y = "Flipper length mm",
+    title = "Bill length vs flipper length in palmers penguins",
+    tag = "B"
+  ) +
+  facet_wrap(facets = vars(sex)) +
+  theme_minimal() +
+  theme(title = element_text(size = 10),
+        axis.title = element_text(size = 10),
+        axis.text = element_text(colour = "navy"))
